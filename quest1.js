26e8940871b086ce395ea34733d51548ed321410
titouwork <titouan.pro.lahely@gmail.com>
2023-10-01 16:22:06 +0200

    second

--- quest1.js
+++ quest1.js
@@ -1,3 +1,6 @@
+"use strict";
+Object.defineProperty(exports, "__esModule", { value: true });
+var assert = require("assert");
 var Person = /** @class */ (function () {
     function Person(name, age) {
         this.name = name;
@@ -14,3 +17,21 @@ var Person = /** @class */ (function () {
 var person1 = new Person('john', 40);
 var person2 = new Person('Mary', 35);
 console.log(person1, person2);
+var BankCustomer = /** @class */ (function () {
+    function BankCustomer(clientName, pass) {
+        this.clientName = clientName;
+        this.pass = pass;
+    }
+    BankCustomer.prototype.getName = function () {
+        return this.clientName;
+    };
+    BankCustomer.prototype.verifyPinInput = function (input) {
+        return input === this.pass;
+    };
+    return BankCustomer;
+}());
+var customer = new BankCustomer('John Doe', '3579');
+assert.equal(typeof customer.getName, 'function');
+assert.equal(typeof customer.verifyPinInput, 'function');
+assert.equal(customer.getName(), 'John Doe');
+assert.ok(customer.verifyPinInput('3579'));
--- quest1.ts
+++ quest1.ts
@@ -1,23 +1,51 @@
-class Person {
-    private name : string;
-    private age : number;
+import * as assert from 'assert';
 
-    constructor (name : string, age : number){
+class Person {
+    private name: string;
+    private age: number;
+
+    constructor(name: string, age: number) {
         this.name = name;
         this.age = age;
     }
 
-    tellMyName() : string{
+    tellMyName(): string {
         return "I am" + this.name;
     }
 
-    tellMyAge() : string{
+    tellMyAge(): string {
         return "I am" + this.age + "years old";
     }
 }
 
 
-const person1  =  new  Person('john', 40);
-const person2  =  new  Person('Mary', 35);
+const person1 = new Person('john', 40);
+const person2 = new Person('Mary', 35);
 
 console.log(person1, person2);
+
+
+class BankCustomer {
+    private clientName: string;
+    private pass: string;
+
+    constructor(clientName: string, pass: string) {
+        this.clientName = clientName;
+        this.pass = pass;
+    }
+
+    public getName(): string {
+        return this.clientName;
+    }
+
+    public verifyPinInput(input: string): boolean {
+        return input === this.pass;
+    }
+}
+
+
+const customer = new BankCustomer('John Doe', '3579');
+assert.equal(typeof customer.getName, 'function');
+assert.equal(typeof customer.verifyPinInput, 'function');
+assert.equal(customer.getName(), 'John Doe');
+assert.ok(customer.verifyPinInput('3579'));
